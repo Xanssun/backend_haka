11c4ee495c3f3325c65a995ef4243cd4745bbf45
Xanssun <115156090+Xanssun@users.noreply.github.com>
2023-11-12 13:06:34 +0600

    комент

---
+++ backend/django_app/api/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from .models import GameStats, Player, MemoryCard, Game
+
+class PlayerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Player
+        fields = ('id', 'username')
+
+class MemoryCardSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MemoryCard
+        fields = ('name', 'image', 'content')
+
+class GameSerializer(serializers.ModelSerializer):
+    memorycards = MemoryCardSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Game
+        fields = ('id', 'memorycards',)
+
+
+class GameStatsSerializer(serializers.ModelSerializer):
+    player = serializers.StringRelatedField()
+    game = serializers.StringRelatedField()
+
+    class Meta:
+        model = GameStats
+        fields = ('id', 'player', 'game', 'time_taken', 'moves_taken')
---
+++ backend/django_app/api/urls.py
@@ -0,0 +1,19 @@
+from django.urls import include, path
+from .views import GameStatsViewSet, PlayerViewSet, MemoryCardViewSet, GameViewSet
+from rest_framework import routers
+
+app_name = 'api'
+router_v1 = routers.DefaultRouter()
+
+router_v1.register('players', PlayerViewSet, basename='player')
+router_v1.register('memorycards', MemoryCardViewSet, basename='memorycard')
+router_v1.register('games', GameViewSet, basename='game')
+router_v1.register('gamestats', GameStatsViewSet, basename='gamestats')
+
+urlpatterns = [
+    path('', include(router_v1.urls)),
+]
+
+
+# api/gamestats/top_players/?gameFieldSize=10?sort=move
+# api/gamestats/top_players/?gameFieldSize=10?sort=time
